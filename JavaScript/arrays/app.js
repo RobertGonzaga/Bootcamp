a758f7489969ce0c9f27892ffc0c37970b3a0bd2
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-19 11:57:06 -0400

    arrays and methods start

---
+++ JavaScript/arrays/app.js
@@ -0,0 +1,35 @@
+// let movieLine = ["Paul", "Bob"];
+
+// movieLine.push("Tom", "Spencer");
+// console.log(movieLine);
+
+// movieLine.pop();
+// console.log(movieLine);
+
+// movieLine.unshift("VIP 1", "VIP 2");
+// console.log(movieLine);
+
+// movieLine.shift();
+// console.log(movieLine);
+
+// let brazilianTeams = ["Cruzeiro", "Flamengo", "Corinthians"];
+// let europeTeams = ["Barcelona", "Real Madrid", "Juventus"];
+
+// const teams = brazilianTeams.concat(europeTeams);
+// console.log(teams);
+
+// teams.reverse();
+// console.log(teams);
+
+// const teamCheck = teams.includes("Palmeiras");
+// console.log(teamCheck);
+
+// console.log(teams.indexOf("Cruzeiro"));
+
+let colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
+
+let warmColors = colors.slice(0, 3);
+console.log(warmColors);
+
+let coolColors = colors.slice(4);
+console.log(coolColors);
