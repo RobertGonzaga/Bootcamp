52727a49794527b06d14c6b8806be992874e36dc
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-19 22:38:45 -0400

    multi-dimensional arrays

--- JavaScript/arrays/app.js
+++ JavaScript/arrays/app.js
@@ -26,16 +26,24 @@
 
 // console.log(teams.indexOf("Cruzeiro"));
 
-let colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
+// let colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
 
-let warmColors = colors.slice(0, 3);
-console.log(warmColors);
+// let warmColors = colors.slice(0, 3);
+// console.log(warmColors);
 
-let coolColors = colors.slice(4);
-console.log(coolColors);
+// let coolColors = colors.slice(4);
+// console.log(coolColors);
 
-colors.splice(3, 1, "magenta");
-console.log(colors);
+// colors.splice(3, 1, "magenta");
+// console.log(colors);
 
-colors.splice(1);
-console.log(colors);
+// colors.splice(1);
+// console.log(colors);
+
+const gameBoard = [
+  ["X", "O", "X"],
+  ["O", null, "X"],
+  ["O", "O", "X"],
+];
+console.log(gameBoard);
+console.log(gameBoard[1][1]);
