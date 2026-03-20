73495eeccfb4ad6e2da39f2adeb4c4cbab423db9
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-20 09:26:08 -0400

    nesting arrays and objects

---
+++ JavaScript/objects/app.js
@@ -0,0 +1,20 @@
+// let pokemon = {
+//   name: "Charizard",
+//   type: "Fire",
+//   power: 2000,
+//   defense: 900,
+//   skills: ["Fire Ball", "Hell Fire", "Flying Kick"],
+// };
+
+// console.log(pokemon);
+// console.log(pokemon.name);
+
+// pokemon.defense = 1200;
+// console.log(pokemon)
+
+let comments = [
+  { username: "Patrick", text: "I see u", votes: 2350 },
+  { username: "Shaun", text: "My name is shaun silly pants jackson", votes: 1250 },
+];
+
+console.log(comments[1].text);
