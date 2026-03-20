8a0e679be23c4ebea23a542c466c0ff8e74d9845
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-20 10:23:29 -0400

    loops

---
+++ JavaScript/loops/app.js
@@ -0,0 +1,48 @@
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i <= 1000; i *= 10) {
+//   console.log(i);
+// }
+
+const pokemons = [
+  "Pikachu",
+  "Charmander",
+  "Bulbasaur",
+  "Squirtle",
+  "Jigglypuff",
+  "Meowth",
+  "Psyduck",
+  "Snorlax",
+  "Eevee",
+  "Mewtwo",
+  "Gengar",
+  "Lapras",
+  "Ditto",
+  "Dragonite",
+  "Lucario",
+  "Greninja",
+  "Charizard",
+  "Blastoise",
+  "Venusaur",
+  "Arcanine",
+];
+
+// for (let i = 0; i < pokemons.length; i++) {
+//   console.log(pokemons[i]);
+// }
+
+for (let i = pokemons.length - 1; i >= 0; i--) {
+  console.log(pokemons[i]);
+}
