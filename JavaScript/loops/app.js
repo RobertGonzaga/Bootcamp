54dfc1f3795b5dddce2ffcea7458b9e7b90bfc35
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-23 10:08:50 -0400

    nested loops

--- JavaScript/loops/app.js
+++ JavaScript/loops/app.js
@@ -16,33 +16,52 @@
 //   console.log(i);
 // }
 
-const pokemons = [
-  "Pikachu",
-  "Charmander",
-  "Bulbasaur",
-  "Squirtle",
-  "Jigglypuff",
-  "Meowth",
-  "Psyduck",
-  "Snorlax",
-  "Eevee",
-  "Mewtwo",
-  "Gengar",
-  "Lapras",
-  "Ditto",
-  "Dragonite",
-  "Lucario",
-  "Greninja",
-  "Charizard",
-  "Blastoise",
-  "Venusaur",
-  "Arcanine",
-];
+// const pokemons = [
+//   "Pikachu",
+//   "Charmander",
+//   "Bulbasaur",
+//   "Squirtle",
+//   "Jigglypuff",
+//   "Meowth",
+//   "Psyduck",
+//   "Snorlax",
+//   "Eevee",
+//   "Mewtwo",
+//   "Gengar",
+//   "Lapras",
+//   "Ditto",
+//   "Dragonite",
+//   "Lucario",
+//   "Greninja",
+//   "Charizard",
+//   "Blastoise",
+//   "Venusaur",
+//   "Arcanine",
+// ];
 
-// for (let i = 0; i < pokemons.length; i++) {
+// // for (let i = 0; i < pokemons.length; i++) {
+// //   console.log(pokemons[i]);
+// // }
+
+// for (let i = pokemons.length - 1; i >= 0; i--) {
 //   console.log(pokemons[i]);
 // }
 
-for (let i = pokemons.length - 1; i >= 0; i--) {
-  console.log(pokemons[i]);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`i is: ${i}`);
+//   for (let j = 1; j <= 5; j++) {
+//     console.log(`    j is: ${j}`);
+//   }
+// }
+
+const names = [
+  ["Aaron", "Chet", "Louise"],
+  ["Paul", "Daniel", "Morgana", "Jinx"],
+  ["Bell", "Tony", "Marie", "Walter"],
+];
+
+for (let i = 0; i < names.length; i++) {
+  for (let j = 0; j < names[i].length; j++) {
+    console.log(`Name: ${names[i][j]}`);
+  }
 }
