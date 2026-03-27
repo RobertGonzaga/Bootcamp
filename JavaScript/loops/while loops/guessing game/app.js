e092d7f5aef8849f98214b7cd0a23109c3732380
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-03-26 23:52:46 -0400

    guessing game bug fixes

--- JavaScript/loops/while loops/guessing game/app.js
+++ JavaScript/loops/while loops/guessing game/app.js
@@ -4,15 +4,18 @@ while (!maxNumber) {
 }
 
 const secretNumber = Math.floor(Math.random() * (maxNumber + 1));
-let guessNumber = parseInt(prompt("Enter your first guess"));
+let guessNumber = prompt("Enter your first guess");
 let attempts = 1;
 
 while (parseInt(guessNumber) !== secretNumber) {
   if (guessNumber === "q") break;
-  else if (guessNumber < secretNumber) {
+  guessNumber = parseInt(guessNumber);
+  if (guessNumber < secretNumber) {
     guessNumber = prompt("Too low, guess again");
-  } else {
+  } else if (guessNumber > secretNumber) {
     guessNumber = prompt("Too high, guess again");
+  } else {
+    guessNumber = prompt("Please enter a valid number");
   }
   attempts++;
 }
