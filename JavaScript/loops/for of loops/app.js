d74fa7749f6a70dca740e093eeb9d736a5dae41f
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-06 23:09:27 -0400

    for of loops

---
+++ JavaScript/loops/for of loops/app.js
@@ -0,0 +1,9 @@
+// const animes = ["Naruto", "Dragon Ball", "Beyblade", "Death Note", "Haikyuu", "Goblin Slayer"];
+
+// for (let single of animes) {
+//   console.log(`I like the ${single} anime`);
+// }
+
+for (char of "Hello World!") {
+  console.log(char);
+}
