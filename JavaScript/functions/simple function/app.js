2017d1cd9fb38a1271e04085c758e539ee360b9c
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-08 11:50:31 -0400

    simple functions and multi arguments functions

---
+++ JavaScript/functions/simple function/app.js
@@ -0,0 +1,21 @@
+function singAlong() {
+  console.log("I wanna be the very best");
+  console.log("Like no one ever was");
+  console.log("To catch them is my real test");
+  console.log("To train them is my cause");
+}
+singAlong();
+
+function greet(firstName, lastName) {
+  console.log(`Hi, welcome ${firstName} ${lastName[0]}.`);
+}
+greet("Jason", "Voorhees");
+
+function repeat(str, times) {
+  let result = "";
+  for (let i = 0; i < times; i++) {
+    result += str;
+  }
+  console.log(result);
+}
+repeat("Hello World! ", 5);
