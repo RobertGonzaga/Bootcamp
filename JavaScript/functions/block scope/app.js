0b2eda3166318ef31e7e1aff7bdd38e0e4c11578
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-14 08:46:54 -0400

    function scope and block scope

---
+++ JavaScript/functions/block scope/app.js
@@ -0,0 +1,9 @@
+let num = 10;
+if (num > 0) {
+  const PI = 3.14;
+  let dice = 6;
+}
+
+console.log(num);
+console.log(PI);
+console.log(dice);
---
+++ JavaScript/functions/scope/app.js
@@ -0,0 +1,19 @@
+// let totalPokemons = 5;
+// function capturePokemons() {
+//   totalPokemons = 10;
+//   console.log(totalPokemons);
+// }
+
+// console.log(totalPokemons);
+// capturePokemons();
+// console.log(totalPokemons);
+
+let pokemon = "Pikachu";
+function changePokemon() {
+  let pokemon = "Charizard";
+  console.log(pokemon);
+}
+
+console.log(pokemon);
+changePokemon();
+console.log(pokemon);
