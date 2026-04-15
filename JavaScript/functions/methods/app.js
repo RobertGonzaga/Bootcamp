a7f25042327876dd3c12da133a92425e4537306d
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-15 10:16:49 -0400

    methods

---
+++ JavaScript/functions/methods/app.js
@@ -0,0 +1,9 @@
+const myMath = {
+  randomizer: function (num) {
+    return Math.floor(Math.random() * num);
+  },
+  PI: 3.14,
+  tempConvert: function (celsius) {
+    return celsius * 1.8 + 32;
+  },
+};
