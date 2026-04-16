60ca4705c6811a79697ea57ba8fd108063bdc977
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-16 09:04:02 -0400

    setTimeout and setInterval

--- JavaScript/callbacks & array methods/arrow functions/app.js
+++ JavaScript/callbacks & array methods/arrow functions/app.js
@@ -15,3 +15,13 @@ const add = (a, b) => a + b;
 // const rollDie = () => (
 //   console.log(Math.floor(Math.random() * 9) + 1)
 // )
+
+//////////////////////////////////////////////
+
+setTimeout(() => {
+  console.log("timeout running");
+}, 3000);
+
+// setInterval(() => {
+//   console.log("Interval Running");
+// }, 2000);
