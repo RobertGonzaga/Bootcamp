0ee212459c8280a629d84548072796a91af5c3b1
RobertGonzaga <robertgonzagaotop@gmail.com>
2026-04-15 23:55:38 -0400

    arrow function implicit returns

--- JavaScript/callbacks & array methods/arrow functions/app.js
+++ JavaScript/callbacks & array methods/arrow functions/app.js
@@ -2,10 +2,16 @@
 //   return a + b;
 // }
 
-const add = (a, b) => {
-  return a + b;
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
 
-const scream = () => {
-  console.log("aaaaaaaaahhhhhh");
-};
+// const scream = () => {
+//   console.log("aaaaaaaaahhhhhh");
+// };
+
+const add = (a, b) => a + b;
+
+// const rollDie = () => (
+//   console.log(Math.floor(Math.random() * 9) + 1)
+// )
